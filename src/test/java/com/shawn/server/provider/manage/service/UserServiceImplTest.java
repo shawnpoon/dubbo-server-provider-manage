1eefa62bd41ebefdd14ce4ae5b139ee8dcbf6fc4
shawnpoon <Shawn-PC>
2016-06-18 11:37:07 +0800

    add some junit test

---
+++ src/test/java/com/shawn/server/provider/manage/service/UserServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.shawn.server.provider.manage.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserServiceImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUser() {
+		fail("Not yet implemented");
+	}
+
+}
