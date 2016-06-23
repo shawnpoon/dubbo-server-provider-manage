89d314fe188989c6757a973e4af094f5080f15ac
shawnpoon <Shawn-PC>
2016-06-23 18:22:34 +0800

    update

--- src/main/java/com/shawn/server/provider/manage/service/UserServiceImpl.java
+++ src/main/java/com/shawn/server/provider/manage/service/UserServiceImpl.java
@@ -1,8 +1,15 @@
 package com.shawn.server.provider.manage.service;
 
+import org.apache.shiro.authc.ExcessiveAttemptsException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.shawn.server.core.shiro.AccountManager;
+import com.shawn.server.core.shiro.AuthenticationUser;
+import com.shawn.server.core.shiro.PermissionManager;
 import com.shawn.server.entity.pojo.User;
 import com.shawn.server.provider.manage.dao.redis.UserRedisDao;
 //import com.shawn.server.provider.manage.dao.redis.UserRedisDao;
@@ -12,10 +19,28 @@ import com.shawn.server.web.api.UserService;
 public class UserServiceImpl implements UserService {
 
 	@Autowired
+	private PermissionManager permissionManager;
+	@Autowired
+	private AccountManager<AuthenticationUser> accountManager;
+	@Autowired
 	private UserRedisDao userRedisDao;
 
 	@Override
 	public User login(String username, String password) {
+		try {
+			boolean isLogin = accountManager.login(username, password);
+		} catch (UnknownAccountException e) {
+			// 账户不存在
+		} catch (ExcessiveAttemptsException e) {
+			// 登录失败次数过多
+		} catch (LockedAccountException e) {
+			// 帐号已被锁定
+		} catch (IncorrectCredentialsException e) {
+			// 登录密码错误
+		} catch (Exception e) {
+
+		}
+
 		User user = new User();
 		user.setUserId(1);
 		user.setUsername(username);
